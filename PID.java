a2c7f1422d0c5693cd2e8d1b9f9cea94881c22e7
ramstick <45892969+ramstick@users.noreply.github.com>
2020-01-13 19:49:19 -0800

    Fixed some errors and added JavaDoc comments

--- PID.java
+++ PID.java
@@ -1,30 +1,39 @@
 public class PID{
-
+    
     double Kp, Kd, Ki;
     double intE, dE;
 
     double end;
     double lastT, lastE;
 
-    boolean done;
-
     double tolerance;
-
+    /**
+     * 
+     * @param Kp - The proportional parameter
+     * @param Kd - The derivative parameter
+     * @param Ki - The integral parameter
+     * @param end - The desired state
+     * @param tolerance - How much it error it will tolerate and call it done.
+     */
     public PID(double Kp, double Kd, double Ki, double end, double tolerance){
         this.Kp = Kp;
         this.Kd = Kd;
         this.Ki = Ki;
         this.end = end;
-        lastError = 0;
+        lastE = 0;
         this.dE = 0;
         intE = 0;
-        done = false;
         this.tolerance = tolerance;
     }
-
+    /**
+     * 
+     * @param current - The current position of the PID controlled
+     * @param timeT - The current time This is cool
+     * @return   - The speed the motor should be set at
+     */
     public double output(double current, double timeT){
-        checkDone(current);
-        if(done){
+        
+        if(checkDone(current)){
             return 0;
         }
         double error = end - current;
@@ -34,8 +43,13 @@ public class PID{
         lastT = timeT;
         return Kp * error + Ki * intE + Kd * (error-lastE) / dt;
     }
-    public void checkDone(double current){
-        done = Math.abs(end-current) < tolerance;
+    /**
+     * 
+     * @param current - The current position
+     * @return - If it has reached its desired end state
+     */
+    public boolean checkDone(double current){
+        return Math.abs(end-current) < tolerance;
     }
 
 }
